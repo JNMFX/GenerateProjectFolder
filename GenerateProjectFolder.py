37a0affcd176103def85a5a028f15004cb8fee91
JNMFX <contact@john-n-mayer.com>
2024-06-24 20:50:50 +1000

    Completed main script.

--- GenerateProjectFolder.py
+++ GenerateProjectFolder.py
@@ -18,8 +18,8 @@ def yes_no_input():
     while True:
         userInput = input()
         userInput = userInput.upper()
-        if userInput in ['YES', "Y", "1", "NO", "N", "0"]:
-            if userInput in ['YES', "Y", "1",]:
+        if userInput in ["YES", "Y", "1", "NO", "N", "0"]:
+            if userInput in ["YES", "Y", "1",]:
                 return True
             else:
                 return False
@@ -45,10 +45,9 @@ while folderCheck(masterPath) == True:
 print(f"Folder exists? {folderCheck(masterPath)}")
 
 masterPath.mkdir()
-shotsPath.mkdir()
 print(f"{masterPath} folder created.")
 
-#Create sequence folders
+#Function to create sequence folders
 def makeSequenceFolders():
     sequenceCount = int(input("Number of sequences: "))
 
@@ -72,9 +71,31 @@ def makeSequenceFolders():
             shotPadding = f"{shot:04}"
             shotFolder = Path(shotsPath) / sequenceFolder / f"sh{shotPadding}"
             shotFolder.mkdir()
+            shotFolder_cg = Path(shotFolder) / 'cg'
+            shotFolder_cg.mkdir()
+            shotFolder_comp_script = Path(shotFolder) / 'comp' / 'script'
+            shotFolder_comp_script.mkdir(parents=True)
+            shotFolder_comp_write = Path(shotFolder) / 'comp' / 'write'
+            shotFolder_comp_write.mkdir()
+            shotFolder_ingest = Path(shotFolder) / 'ingest'
+            shotFolder_ingest.mkdir()
+            shotFolder_lighting = Path(shotFolder) / 'lighting'
+            shotFolder_lighting.mkdir()
+            shotFolder_plate = Path(shotFolder) / 'plate'
+            shotFolder_plate.mkdir()
+            shotFolder_rotoPaint = Path(shotFolder) / 'rotoPaint'
+            shotFolder_rotoPaint.mkdir()
             print(f"{shotFolder} created.")
 
-makeSequenceFolders()
+#Create SHOTS folders
+print("Create Shots folder?")
+shotsCheck = yes_no_input()
+
+if shotsCheck == True:
+    shotsPath.mkdir()
+    makeSequenceFolders()
+else:
+    print("Shots folder not created.")
 
 #Generate globals
 print("Create Globals folder? y/n")
@@ -82,19 +103,21 @@ globalsCheck = yes_no_input()
         
 if globalsCheck == True:
     globalsFolder = Path(masterPath) / 'GLOBALS'
-    globalsFolder.mkdir
+    globalsFolder.mkdir()
     globals_cg_char = Path(globalsFolder) / 'cg' / 'char'
     globals_cg_char.mkdir(parents=True)
     globals_cg_env = Path(globalsFolder) / 'cg' / 'env'
-    globals_cg_env.mkdir
+    globals_cg_env.mkdir()
     globals_cg_fx = Path(globalsFolder) / 'cg' / 'fx'
-    globals_cg_fx.mkdir
-    globals_cg_library = Path(globalsFolder) / 'cg' / 'library'
-    globals_cg_library.mkdir
+    globals_cg_fx.mkdir()
+    globals_cg_library_HDRI = Path(globalsFolder) / 'cg' / 'library' / 'HDRI'
+    globals_cg_library_HDRI.mkdir(parents=True)
+    globals_cg_library_textures = Path(globalsFolder) / 'cg' / 'library' / 'textures'
+    globals_cg_library_textures.mkdir()
     globals_cg_prop = Path(globalsFolder) / 'cg' / 'prop'
-    globals_cg_prop.mkdir
+    globals_cg_prop.mkdir()
     globals_Dev = Path(globalsFolder) / 'Dev'
-    globals_Dev.mkdir
+    globals_Dev.mkdir()
     print("Globals folder created.")
     
 else:
@@ -104,10 +127,22 @@ print("Generate Editorial folder?")
 editorialCheck = yes_no_input()
 
 if editorialCheck == True:
-    globalsFolder = Path(masterPath) / 'EDITORIAL'
-    globalsFolder.mkdir
+    editorialFolder = Path(masterPath) / 'EDITORIAL'
+    editorialFolder.mkdir()
+    editorialFolder_audio_music = Path(editorialFolder) / 'audio' / 'music'
+    editorialFolder_audio_music.mkdir(parents=True)
+    editorialFolder_audio_sfx = Path(editorialFolder) / 'audio' / 'sfx'
+    editorialFolder_audio_sfx.mkdir()
+    editorialFolder_audio_vo = Path(editorialFolder) / 'audio' / 'vo'
+    editorialFolder_audio_vo.mkdir()
+    editorialFolder_DELIVERY = Path(editorialFolder) / 'DELIVERY'
+    editorialFolder_DELIVERY.mkdir()
+    editorialFolder_footage = Path(editorialFolder) / 'footage'
+    editorialFolder_footage.mkdir()
+    editorialFolder_Resolve = Path(editorialFolder) / 'Resolve'
+    editorialFolder_Resolve.mkdir()
 
 else:
     print("Editorial folder not created")
 
-input("Folders created. Press enter to finish.")
+input("Task complete. Press enter to finish.")
